ef8308015ca43340471335176da825d136d7b01a
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-17 14:07:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,4 +1,10 @@
-class CashRegister
+class Cash_Register
+  attr_accessor :total
+  
+  def initialize(total = 0)
+    @total = total
+  end
 end
 
-class CashRegister_with_discount
+class Cash_Register_with_discount
+end
